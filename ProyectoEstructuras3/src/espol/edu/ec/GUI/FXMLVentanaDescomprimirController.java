2cfaffd3be4608cb336b43c5fedd320c49346299
Kevin Vásconez <kvasconezl@gmail.com>
2017-08-07 20:37:11 -0500

    Removed unused imports

--- ProyectoEstructuras3/src/espol/edu/ec/GUI/FXMLVentanaDescomprimirController.java
+++ ProyectoEstructuras3/src/espol/edu/ec/GUI/FXMLVentanaDescomprimirController.java
@@ -29,8 +29,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
